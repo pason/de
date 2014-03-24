f38423ea1f4c0591c5426328bed92f0d5de795a6
Slawomir <slawomir@tempity.com>
2014-03-24 23:16:58 +0100

    secret key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dancevent::Application.config.secret_key_base = '37c28ff6d7009479d91f93583a02b8ceac05d95db1f432a16e13ced0b0af0bdc882bc2147c192bafef8a4883505c38d389e20b503e0b1cc5c92b60a1f559b4a5'
